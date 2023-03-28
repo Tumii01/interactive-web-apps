e45e23843c5a1fb78155c0006144445f957ca867
Tumii01 <tumisangnke@gmail.com>
2023-03-28 13:48:00 +0200

    Comments configuring regarding console

--- IWA1/scripts.js
+++ IWA1/scripts.js
@@ -1,5 +1,39 @@
+// This is the firstname of the user
+const user = 'John'
+
+// This is the firstname of the user
+const surname = 'Smith'
+console.log(user, surname)
+
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+var date = "10/07/2014";
+var date = "date"
+console.log(date)
+
+/* 
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+
+console.warn('Security scan starting')
+
+// It is important to let user know when they can close the page  
+
+console.info('Please wait for scan to complete before closing the browser.')
+
+
+
+//Counter Configurations
+
 const MAX_NUMBER = 15
 const MIN_NUMBER = -5
+
 
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
@@ -19,7 +53,7 @@ const subtractHandler = () => {
 }
 
 const addHandler = () => {
-    const newValue = parseInt(number.value) + 1
+    const newValue = parseInt(number.value) + 1 //Changing string to Number
     number.value = newValue
 
     if (subtract.disabled === true) {
@@ -30,6 +64,7 @@ const addHandler = () => {
     }
 }
 
+//Clicking
 
 subtract.addEventListener('click', subtractHandler)
 
