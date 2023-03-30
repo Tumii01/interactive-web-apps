450fdb43d502de15f405c6eadd33a4df04a3ff15
Tumii01 <tumisangnke@gmail.com>
2023-03-30 13:09:27 +0200

    changes to code

--- IWA4/scripts.js
+++ IWA4/scripts.js
@@ -1,8 +1,8 @@
-let date = "2050" 
-let occupation= "parent"
+let date = 2050
+let status= "parent"
 let count = 0
 
-if (date = "2050") {
+if (date = 2050) {
 	console.log("January", 'New Year`s Day')
 	console.log("March", 'Human Rights Day')
 	let date = 'April'
@@ -10,7 +10,7 @@ if (date = "2050") {
 	console.log(date, 'Freedom Day')
 	 count = count + 4
 
-	if (occupation === "student") {
+	if (status === "student") {
 	  console.log('June', 'Youth Day')
 		 count = count + 1
   }
@@ -21,7 +21,7 @@ if (date = "2050") {
 	console.log(date, 'Day of Reconciliation')
 	 count = count + 3
 
-	if (occupation === "parent") {
+	if (status === "parent") {
 	  console.log(date, 'Christmas Day')
 		 count = count + 1
   }
@@ -31,6 +31,6 @@ if (date = "2050") {
 }
 
 
-console.log('Your status is:', occupation)
+console.log('Your status is:', status)
 console.log('The year is:', date)
 console.log('The total holidays is:', count)
