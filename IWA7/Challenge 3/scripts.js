cab453d6a4ba12dbebe1c44eca5104574f487d25
Tumii01 <tumisangnke@gmail.com>
2023-04-05 16:28:08 +0200

    code update

--- IWA7/Challenge 3/scripts.js
+++ IWA7/Challenge 3/scripts.js
@@ -1,17 +1,17 @@
-const leoName = 'Leo';
-const leoSurname = 'Musvaire';
-const leoBalance = -9394;
+const leoName = 'Leo'
+const leoSurname = 'Musvaire     '
+const leoBalance = '-9394'
 
-const sarahName = 'Sarah';
-const sarahSurname = 'Kleinhans';
-const sarahBalance = -4582.21;
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.2100011'
 
 const divider = '----------------------------------';
 
 // Only change below this line
 
-const leo = `${leoName} ${leoSurname}  `+ ` (Owed: R ${(-leoBalance).toFixed(2)})`;
-const sarah = `${sarahName} ${sarahSurname}   ` + `(Owed: R ${(-sarahBalance).toFixed(2)})`;
+const leo = `${leoName} ${leoSurname.trim()}  `+ ` (Owed: R ${(-leoBalance).toFixed(2)})`;
+const sarah = `${sarahName.trim()} ${sarahSurname}   ` + `(Owed: R ${(-sarahBalance).toFixed(2)})`;
 const owed = (-leoBalance - sarahBalance).toLocaleString( { style: 'currency' });
 const total = `  Total amount owed: R ${owed}  `;
 
